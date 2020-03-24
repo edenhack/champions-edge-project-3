c9bd49a6b352c33fe53a7638d5aa08f2956bd082
Kingsleigh Crown <20748544+edenhack@users.noreply.github.com>
2020-03-24 12:39:39 -0500

    Project failure, api no longer allows me to request the data i need for the sight without being a paid developer, all work on api halted

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,1 +1,12 @@
 import axios from "axios";
+
+const BASEURL = `https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/`;
+const APIKEY = "?api_key=RGAPI-bd437fc1-2419-479e-a859-868287fd1dca";
+
+summonerName
+
+export default {
+  search: function (summonerName) {
+    return axios.get(BASEURL + summonerName + APIKEY);
+  }
+};
